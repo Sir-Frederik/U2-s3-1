61ea217d57620fbb33a67d3a7a0184330bea5a4e
Federico <brunetti.federico.13@gmail.com>
2025-04-28 17:05:40 +0200

    aggiustato metodo confronto esercizio 1

--- esercizio.js
+++ esercizio.js
@@ -7,12 +7,12 @@ class User {
   }
 
   ageComparison(y) {
-    if (this.age > y) {
-      return `Il signor ${this.firstName} è più vecchio dell'altro tizio`;
+    if (this.age > y.age) {
+      return `Il signor ${this.firstName} è più vecchio di ${y.firstName}`;
     } else if (this.age < y) {
-      return `Il signor ${this.firstName} è più giovane dell'altro tizio`;
+      return `Il signor ${this.firstName} è più giovane di ${y.firstName}`;
     } else {
-      return `Il signor ${this.firstName} è  coetaneo con l'altro tizio`;
+      return `Il signor ${this.firstName} è  coetaneo con di ${y.firstName}`;
     }
   }
 }
@@ -22,6 +22,6 @@ const utente1 = new User("Ciccio", "Pasticcio", 38, "Calisota");
 const utente2 = new User("Marpo", "Cirop", 79, "Quebec");
 const utente3 = new User("Donaldo", "Trampolo", 79, "Nuova Yorka");
 
-console.log(utente1.ageComparison(utente2.age));
-console.log(utente2.ageComparison(utente1.age));
-console.log(utente3.ageComparison(utente2.age));
+console.log(utente1.ageComparison(utente2));
+console.log(utente2.ageComparison(utente1));
+console.log(utente3.ageComparison(utente2));
